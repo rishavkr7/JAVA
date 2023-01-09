4b35179b63d37e9f41f94ff62f0ed8ab657b937f
rishavkr7 <rishav0797@gmail.com>
2023-01-09 18:51:09 +0530

    intial commit

--- GETTING_START/CompareThreeNumber.java
+++ GETTING_START/CompareThreeNumber.java
@@ -12,18 +12,19 @@ public class CompareThreeNumber {
 
         if(num1>num2){
             if(num1>num3){
-                System.out.println(num1+" is greater than two numbers");
+                System.out.println(num1+" is greater than "+num2+" and "+num3);
             }
             else{
-                System.out.println(num3+ " is grater");            }
+                System.out.println(num3+ " is grater than "+num1+" and "+num2);            
+            }
         }
         else
         {
             if(num2>num3){
-                System.out.println(num2+ " is greater"); 
+                System.out.println(num2+ " is greater than"+num1+" and "+num3); 
             }
             else{
-                System.out.println(num3+ " is greater");
+                System.out.println(num3+ " is grater than "+num1+" and "+num2); 
             }
         }
     }
