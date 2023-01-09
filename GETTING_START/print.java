58152fadb46bdbe3f2d3adbf5912f77914c3c81f
rishavkr7 <rishav0797@gmail.com>
2023-01-09 16:25:47 +0530

    first commit

---
+++ GETTING_START/print.java
@@ -0,0 +1,15 @@
+package GETTING_START;
+class print{
+    public static void main (String args[]){
+        System.out.println("Hello JAVA");
+        System.out.println();
+        System.out.println("PRINTING Z BY MANUALLY");
+        System.out.println("******");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("******");
+
+    }
+}
