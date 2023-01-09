c2b4b29ab6037c6740b67f0174858b04d805a0be
rishavkr7 <rishav0797@gmail.com>
2023-01-09 17:58:19 +0530

    intial commit

--- GETTING_START/EvenOdd.java
+++ GETTING_START/EvenOdd.java
@@ -1,10 +1,21 @@
 package GETTING_START;
 import java.util.*;
 public class EvenOdd {
-  public static void main(String[] args){
+ 
+public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
+    System.out.print("ENTER TERMS: ");
+    int terms=scn.nextInt();
+    for(int m=1;m<=terms;m++){
     System.out.print("Enter Number: ");
-    
-    scn.close();
+    int num = scn.nextInt();
+    if(num%2==0){
+        System.out.println("It is Even Number");
+    }
+    else{
+        System.out.println("It is odd Number");
+    }
+   }
+   scn.close();
   }  
 }
