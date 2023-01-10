e0e13aab816f29f9e54cab4aaaaabd1e3a6487e7
rishavkr7 <rishav0797@gmail.com>
2023-01-10 19:27:45 +0530

    intial commit

--- LOOP/SumOfNaturalNumber.java
+++ LOOP/SumOfNaturalNumber.java
@@ -6,11 +6,12 @@ public class SumOfNaturalNumber {
         System.out.print("Enter the TERMS: ");
         int terms=scn.nextInt();
         for(int i=1;i<=terms;i++){
-        
+        System.out.print("Enter the number: ");
         int num=scn.nextInt();
         int sum=0;
         sum=(num*(num+1))/2;
-        System.out.println(sum);
+        System.out.println("THE SUMMESSION OF 1 TO "+num+" IS: "+sum);
     }
+    scn.close();
     }
 }
