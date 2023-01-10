7492caf979bba06997c9f4cdf889386ed35b054b
rishavkr7 <rishav0797@gmail.com>
2023-01-10 19:23:44 +0530

    intial commit

--- LOOP/SumOfNaturalNumber.java
+++ LOOP/SumOfNaturalNumber.java
@@ -3,10 +3,14 @@ import java.util.*;
 public class SumOfNaturalNumber {
     public static void main(String[] args){
         Scanner scn = new Scanner (System.in);
+        System.out.print("Enter the TERMS: ");
+        int terms=scn.nextInt();
+        for(int i=1;i<=terms;i++){
+        
         int num=scn.nextInt();
         int sum=0;
         sum=(num*(num+1))/2;
         System.out.println(sum);
-        scn.close();
+    }
     }
 }
