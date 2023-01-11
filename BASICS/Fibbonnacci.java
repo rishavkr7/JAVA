d772c0a94cfc9d94631d95cc6c23abb91e0441b0
rishavkr7 <rishav0797@gmail.com>
2023-01-11 16:33:55 +0530

    intial commit

---
+++ BASICS/Fibbonnacci.java
@@ -0,0 +1,20 @@
+package BASICS;
+import java.util.*;
+public class Fibbonnacci{
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        int num=scn.nextInt();
+        // System.out.println("0,1");
+        int first=0;
+        int second=1;
+        int i=1;
+        while(i<=num){
+            int third=first+second;
+            System.out.println(third);
+            first=second;
+            second=third;
+            i++;
+            
+        }
+    }
+}
