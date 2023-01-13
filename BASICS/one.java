8e2d42819f216a4a5d89ec72570f4d3db8bda1c7
rishavkr7 <rishav0797@gmail.com>
2023-01-13 18:51:57 +0530

    intial commit

--- BASICS/one.java
+++ BASICS/one.java
@@ -5,10 +5,12 @@ public class one{
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num=scn.nextInt();
-
+        int reverse=0 , rem;
         while(num!=0)
         {
-           
+           rem=num%10;
+           reverse=reverse*10+rem;
+           num=num/10;
         }
 
     scn.close();
